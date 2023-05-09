38213146ccfeacfb33790d8f8cd139d6c426acef
solenwa <solen.wanono@gmail.com>
2023-05-09 18:06:51 +0200

    Lab done without delete button working

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import contacts from './contacts.json'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstFive, setFirstFive] = useState(contacts.slice(0,5));
+  const [allContacts, setAllContacts] = useState(contacts)
+
+  const addRandomContact = () => {
+    const randomIndex = Math.floor(Math.random() * allContacts.length)
+    const randomContact = allContacts[randomIndex]
+    const isDuplicateContact = firstFive.some(contact => contact.id === randomContact.id)
+    if (!isDuplicateContact) {
+      const updatedContacts = [...firstFive, randomContact];
+      setFirstFive(updatedContacts);
+    }
+  }
+
+  const sortByPopularity = () => {
+    const sortedByPopularity = [...firstFive].sort((a, b) => b.popularity - a.popularity);
+    setFirstFive(sortedByPopularity)
+  }
+
+  const sortByName = () => {
+    const sortedByName = [...firstFive].sort((a, b) => a.name.localeCompare(b.name));
+    console.log(sortedByName)
+    setFirstFive(sortedByName)
+  }
+
+  const deleteButton = (contactId) => {
+    const updatedContacts = [...firstFive].filter()
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='App'>
+      <h1>IronContacts</h1>
+      <button onClick={addRandomContact}>Add Random Contact</button>
+      <button onClick={sortByPopularity}>Sort By Popularity</button>
+      <button onClick={sortByName}>Sort By Name</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+            {firstFive.map(currentContact => {
+              return (
+                <tr>
+                  <td><img src={currentContact.pictureUrl} alt="Contact's photo" height='150px'></img></td>
+                  <td>{currentContact.name}</td>
+                  <td>{Math.round(currentContact.popularity*100) / 100}</td>
+                  {currentContact.wonOscar ? <td> 🏆</td> : <td></td>}
+                  {currentContact.wonEmmy ? <td> 🏆</td> : <td></td>}
+                  <button onClick={() => deleteButton(currentContact.id)}>Delete</button>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
-
 export default App
